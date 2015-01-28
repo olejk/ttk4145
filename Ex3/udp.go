9de659f29e18931b82ba5b1147d28d09938f02ee
olejk <olejk@stud.ntnu.no>
2015-01-28 10:12:44 +0100

    test

--- Ex3/udp.go
+++ Ex3/udp.go
@@ -2,13 +2,23 @@ package main
 
 import(
 	"fmt"
-	"runtime"
 	"net"
 )
 
-const udpPort = 20021
+
+const (
+	port = ":30000"
+)
 
 func main() {
-
-
+	buff := make([]byte, 1024)
+	addr, _ := net.ResolveUDPAddr("udp", port)
+	sock, _ := net.ListenUDP("udp", addr)
+	for {
+		_,_, err:=sock.ReadFromUDP(buff)
+		if err != nil {
+			fmt.Println(err)
+		} 
+		fmt.Println(string(buff[:]))
+	}
 }
