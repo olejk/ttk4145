46adb2dcd464df84cd1989741cefcc9282faa071
olejk <olejk@stud.ntnu.no>
2015-01-28 11:54:45 +0100

    prøver å sende/motta

--- Ex3/udp.go
+++ Ex3/udp.go
@@ -3,16 +3,19 @@ package main
 import(
 	"fmt"
 	"net"
+	"time"
 )
 
 
 const (
-	port = ":30000"
+	host = "129.241.187.255"
+	udpPort = "20021"
+	port_t = "30000"
 )
 
-func main() {
+func udpReceive(port int) {
 	buff := make([]byte, 1024)
-	addr, _ := net.ResolveUDPAddr("udp", port)
+	addr, _ := net.ResolveUDPAddr("udp", ":" + port)
 	sock, _ := net.ListenUDP("udp", addr)
 	for {
 		_,_, err:=sock.ReadFromUDP(buff)
@@ -21,4 +24,27 @@ func main() {
 		} 
 		fmt.Println(string(buff[:]))
 	}
+}
+
+func udpSend() {
+	raddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(host, udpPort))
+	if err != nil {
+		fmt.Println("Failed to resolve address for: " + udpPort)
+	}
+	
+	conn, err := DialUDP("udp", nil, raddr)	
+	/*if err != nil {
+		fmt.Println()
+	}*/
+	
+	udpReceive(udpPort)
+	for {
+		time.Sleep(1000*time.Millisecond)
+		conn.Write([]byte("bananapancakes")
+	}
+
+}
+
+func main() {
+	udpSend()
 }
