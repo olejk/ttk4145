5a9e6e9cb49b53bc57048aee99e1429624eb24ed
olejk <olejk@stud.ntnu.no>
2015-02-04 10:27:57 +0100

    attempted fix

--- Ex4/udp.go
+++ Ex4/udp.go
@@ -4,6 +4,8 @@ import(
 	"fmt"
 	"net"
 	"time"
+	"encoding/json"
+	"os"
 )
 
 
@@ -13,39 +15,55 @@ const (
 	port_t = "30000"
 )
 
-func udpReceive(port string) {
+type Message struct {
+	str1	string
+	str2	string
+	str3	string
+}
+
+func udpReceive(port string, m []byte) {
 	buff := make([]byte, 1024)
+	var mess_rec []Messages
 	addr, _ := net.ResolveUDPAddr("udp", ":" + port)
 	sock, _ := net.ListenUDP("udp", addr)
 	for {
 		_,_, err:=sock.ReadFromUDP(buff)
 		if err != nil {
-			fmt.Println(err)
+			fmt.Println("Error UDP read: ", err)
 		} 
-		fmt.Println(string(buff[:]))
+		err := json.Unmarshal(m, &mess_rec)
+		fmt.Println("%+v", mess_rec)
 	}
 }
 
-func udpSend() {
+func udpSend(m []byte) {
 	raddr, err := net.ResolveUDPAddr("udp", net.JoinHostPort(host, udpPort))
 	if err != nil {
 		fmt.Println("Failed to resolve address for: " + udpPort)
 	}
-	
 	conn, err := net.DialUDP("udp", nil, raddr)	
 	if err != nil {
-		fmt.Println("EREREREROROOROROR")
+		fmt.Println("Error dial: ", err)
 	}
 	
 	go udpReceive(udpPort)
 	for {
 		time.Sleep(1000*time.Millisecond)
-		conn.Write([]byte("bananapancakes"))
+		conn.Write(m)
 		fmt.Println("Msg sent")	
 	}
 
 }
 
 func main() {
-	udpSend()
+	messages := Message{
+		str1:	"string 1",
+		str2:	"string 2",
+		str3:	"string 3",	
+	}
+	b, err := json.Marshal(messages)
+	if err != nil {
+		fmt.Println("error: ", err)
+	}
+	udpSend(messages)
 }
